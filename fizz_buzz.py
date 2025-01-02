32df986001749516b9fce5955d79158afaa6df7a
PolarBeartic <brian.rooney@uconn.edu>
2025-01-01 20:19:36 -0500

    First Commit- Initial File

---
+++ fizz_buzz.py
@@ -0,0 +1,32 @@
+###############################################################################
+# Collaboration                                                               #
+# -------------                                                               #
+# You can collaborate with up to 3 classmates (for a total of 4 students per  #
+# group). If you do so, remember not to share code directly. Discussions are  #
+# fine, code sharing is not. Also note that all have to submit individually.  #
+#                                                                             #
+# Enter any collaborators here:                                               #
+# Collaborator 1:                                                             #
+# Collaborator 2:                                                             #
+# Collaborator 3:                                                             #
+###############################################################################
+
+def fizz_buzz(start, finish):
+    """Print 'fizz' and 'buzz' when necessary"""
+    #Loop through nums between start and finish
+    for num in range(start, finish+1):
+
+        #Initialize num_string as num in type string
+        num_string=num.__str__()
+        
+        #Print num if no conditions for 'fizz' or 'buzz' are met
+        if not (num%3==0 or num%5==0 or num_string.__contains__("3") or num_string.__contains__("5")):
+            print(num)
+        #Check met conditions, printing 'fizzbuzz', 'fizz', or 'buzz' when needed
+        elif((num%3==0 or num_string.__contains__("3")) and (num%5==0 or num_string.__contains__("5"))):
+            print('fizzbuzz')
+        else:
+            if num%3==0 or num_string.__contains__("3"):
+                print('fizz')
+            if num%5==0 or num_string.__contains__("5"):
+                print('buzz')
