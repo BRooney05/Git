0d891a4c0a933226b29cd02dd066fe65db621980
PolarBeartic <brian.rooney@uconn.edu>
2025-05-23 10:03:00 -0400

    removed comment text

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,16 +1,3 @@
-###############################################################################
-# Collaboration                                                               #
-# -------------                                                               #
-# You can collaborate with up to 3 classmates (for a total of 4 students per  #
-# group). If you do so, remember not to share code directly. Discussions are  #
-# fine, code sharing is not. Also note that all have to submit individually.  #
-#                                                                             #
-# Enter any collaborators here:                                               #
-# Collaborator 1:                                                             #
-# Collaborator 2:                                                             #
-# Collaborator 3:                                                             #
-###############################################################################
-
 def fizz_buzz(start, finish):
     """Print 'fizz' and 'buzz' when necessary"""
     #Loop through nums between start and finish
